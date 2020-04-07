0bca47e1845c8c4113473becc6b84eec45be38ca
tcsl-lsy-zhuliangshuang <FatMaJin_Buu@126.com>
2020-04-07 13:34:38 +0800

    基础提交

---
+++ candy-rebate-web/src/main/java/cn/com/mjb/candyrebateweb/CandyRebateWebApplication.java
@@ -0,0 +1,9 @@
+package cn.com.mjb.candyrebateweb;
+
+import org.springframework.boot.SpringApplication;
+
+public class CandyRebateWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CandyRebateWebApplication.class, args);
+    }
+}
